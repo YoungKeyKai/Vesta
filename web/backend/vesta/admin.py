f409dc21429c4b3de774abb752f9ed6c4a28c072
FaizaanRehman <FaizaanRehman@users.noreply.github.com>
2022-07-10 17:30:51 -0400

    Configured initial API endpoints

--- web/backend/vesta/admin.py
+++ web/backend/vesta/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import *
+
+class UserUploadAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'uploadtime', 'content')
+
+class UserSettingsAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'visible', 'chatOn')
+
+class UserPreferencesAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'pricerange', 'timerange', 'location', 'rating')
+
+class ListingPropertyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city', 'country')
+
+class ListingListingAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'propertyID', 'unit', 'duration', 'rate', 'utilities', 'floorplan', 'proof', 'status')
+
+class ListingInterestAdmin(admin.ModelAdmin):
+    list_display = ('buyer', 'seller', 'listing', 'status')
+
+class ListingFlaggedListingAdmin(admin.ModelAdmin):
+    list_display = ('listing', 'flagger', 'timestamp', 'type')
+
+class MessagingBlocksAdmin(admin.ModelAdmin):
+    list_display = ('blocker', 'blocked', 'timestamp')
+
+class MessagingChatAdmin(admin.ModelAdmin):
+    list_display = ('user1', 'user2', 'history')
+    
 
 # Register your models here.
+admin.site.register(UserUpload, UserUploadAdmin)
+admin.site.register(UserSettings, UserSettingsAdmin)
+admin.site.register(UserPreferences, UserPreferencesAdmin)
+admin.site.register(ListingProperty, ListingPropertyAdmin)
+admin.site.register(ListingListing, ListingListingAdmin)
+admin.site.register(ListingInterest, ListingInterestAdmin)
+admin.site.register(ListingFlaggedListing, ListingFlaggedListingAdmin)
+admin.site.register(MessagingBlocks, MessagingBlocksAdmin)
+admin.site.register(MessagingChat, MessagingChatAdmin)
--- web/backend/vesta/views.py
+++ web/backend/vesta/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
 
 # Create your views here.
+
+class UserUploadView(viewsets.ModelViewSet):
+    serializer_class = UserUploadSerializer
+    queryset = UserUpload.objects.all()
+
+class UserSettingsView(viewsets.ModelViewSet):
+    serializer_class = UserSettingsSerializer
+    queryset = UserSettings.objects.all()
+
+class UserPreferencesView(viewsets.ModelViewSet):
+    serializer_class = UserPreferencesSerializer
+    queryset = UserPreferences.objects.all()
+
+class ListingPropertyView(viewsets.ModelViewSet):
+    serializer_class = ListingPropertySerializer
+    queryset = ListingProperty.objects.all()
+
+class ListingListingView(viewsets.ModelViewSet):
+    serializer_class = ListingListingSerializer
+    queryset = ListingListing.objects.all()
+
+class ListingInterestView(viewsets.ModelViewSet):
+    serializer_class = ListingInterestSerializer
+    queryset = ListingInterest.objects.all()
+
+class ListingFlaggedListingView(viewsets.ModelViewSet):
+    serializer_class = ListingFlaggedListingSerializer
+    queryset = ListingFlaggedListing.objects.all()
+
+class MessagingBlocksView(viewsets.ModelViewSet):
+    serializer_class = MessagingBlocksSerializer
+    queryset = MessagingBlocks.objects.all()
+
+class MessagingChatView(viewsets.ModelViewSet):
+    serializer_class = MessagingChatSerializer
+    queryset = MessagingChat.objects.all()

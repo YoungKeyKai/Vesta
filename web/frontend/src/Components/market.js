2ed65330e594ea5d15b80861e4a4fe109fc03708
YoungKey <cai20002011@hotmail.com>
2022-07-12 15:43:29 -0400

    create dummy market page

--- web/frontend/src/Components/market.js
+++ web/frontend/src/Components/market.js
@@ -1,18 +1,9 @@
-import { Button } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-
-function Market() {
-  return (
-    <div>
-      <p>
-        This is the next page!
-      </p>
-
-      <Button variant="contained" startIcon={<DeleteIcon />} onClick={() => alert('clicked')}>
-        Contained Button
-      </Button>
-    </div>
-  );
+export default function Market() {
+    return (
+        <div className='Market'>
+            <p>
+                This is the market place page!
+            </p>
+        </div>
+    );
 }
-
-export default Market;

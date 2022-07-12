b928ee9953bb00da0371c0afd78f762bf7ee0161
YoungKey <cai20002011@hotmail.com>
2022-07-12 11:12:19 -0400

    Move background css style into app.js

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,21 +1,36 @@
 import {
-  Routes,
-  Route,
-  BrowserRouter as Router
+    Routes,
+    Route,
+    BrowserRouter as Router
 } from "react-router-dom";
 
 import Market from './Components/market';
 import Home from './Components/home';
 
 function App() {
-  return (
-    <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/market" element={<Market />} />
-        </Routes>
-    </Router>
-  );
+    return (
+        <div
+            className="HomePage"
+            style={{
+                textAlign: "center",
+                backgroundColor: "#E1E7FF",
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "calc(10px + 2vmin)",
+                color: "black",
+            }}
+        >
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/market" element={<Market />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;

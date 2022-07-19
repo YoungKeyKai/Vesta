77a49eb35b5522e4f57c1f8ba88dddb2c6d989ae
Brian Jiang <brianjiang2200@gmail.com>
2022-07-18 21:41:06 -0400

    Init Market Page

--- web/frontend/src/Components/market.js
+++ web/frontend/src/Components/market.js
@@ -1,9 +1,31 @@
+import React, { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent'
+import Pagination from '@mui/material/Pagination';
+
+import '../css/market.css'
+
 export default function Market() {
+
+    const [tiles, setTiles] = useState([
+        {name: 'Fergus House'},
+        {name: 'Blair House'},
+        {name: 'ICON'}
+    ]);
+
     return (
         <div className='Market'>
-            <p>
-                This is the market place page!
-            </p>
+            <h1>Market</h1>
+            <div className='market-listings'>
+                {tiles.map( (tile, index) => (
+                    <Card className='market-listing-card' key={index}>
+                        <CardContent>
+                            <h3>{tile.name}</h3>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+            <Pagination count={10} color="secondary"/>
         </div>
     );
 }

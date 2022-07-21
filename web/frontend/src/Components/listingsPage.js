86ced5d6b6005775177ced2498cbd68ef64f2aed
YoungKey <cai20002011@hotmail.com>
2022-07-21 15:53:33 -0400

    Add fundamentals of listing page: - Add embedded Google Map - Add API requests to find the listing info

--- web/frontend/src/Components/listingsPage.js
+++ web/frontend/src/Components/listingsPage.js
@@ -1,23 +1,56 @@
-import '../css/listingsPage.css';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
 
-import { googleMapsAPIKey } from '../constants';
+import '../css/listingsPage.css';
+import { googleMapsAPIKey, terms } from '../constants';
 
 export default function ListingsPage() {
-    const TEMP_APIKEY = 'AIzaSyClRBH_pT4waqp3BxnTtJw_7z-6clxz_HU';
-    const TEMP_LOCATION = '110+University+Ave+West,Waterloo+Ontario';
+    const [listing, setListing] = useState({});
+    const [property, setProperty] = useState({});
+    const [googleMapsAddr, setGoogleMapsAddr] = useState('');
+    const [searchParams] = useSearchParams();
+
+    const formatAddr = (addr, city, country) => `${addr.replaceAll(/ +/g, '+')},${city}+${country}`;
+
+    useEffect(() => {
+        const getProperty = id => axios
+            .get(`/api/listingproperties/${id}`)
+            .then((res) => {
+                const data = res.data;
+                setProperty(data);
+                setGoogleMapsAddr(formatAddr(data.address, data.city, data.country));
+            });
+
+        const getListing = () => axios
+            .get(`/api/listinglistings/${searchParams.get('id')}`)
+            .then((res) => {
+                const data = res.data;
+                setListing(data);
+                getProperty(data.propertyID);
+            });
+
+        getListing();
+    }, [searchParams])
 
     return (
         <div className='ListingsPage'>
             <div className='LeftColumn'>
                 <div className='AddressAndPrice'>
-                    AddressAndPrice
+                    <h1>{property.name} {listing.unit}</h1>
                 </div>
-                <iframe
-                    title='GoogleMapsEmbed'
-                    className='GoogleMapsEmbed'
-                    referrerpolicy="no-referrer-when-downgrade"
-                    src={`https://www.google.com/maps/embed/v1/place?key=${googleMapsAPIKey}&q=${TEMP_LOCATION}`}>
-                </iframe>
+                <div className="map-wrapper">
+                    {
+                        googleMapsAddr ?
+                            <iframe
+                                title='GoogleMapsEmbed'
+                                className='google-maps-embed'
+                                referrerPolicy="no-referrer-when-downgrade"
+                                src={`https://www.google.com/maps/embed/v1/place?key=${googleMapsAPIKey}&q=${googleMapsAddr}`}
+                            /> :
+                            <h2>{terms.loading}</h2>
+                    }
+                </div>
             </div>
             <div className='RightColumn'>
             </div>

3fb5e96ccf3d63774106f8cfd11afa1ed6e5fe62
Brian Jiang <brianjiang2200@gmail.com>
2023-01-11 13:11:59 -0500

    Integrate API with next.js migration

--- web/frontend/next.config.js
+++ web/frontend/next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_ROOT}/api/:path*/`,
+      }
+    ]
+  }
 };

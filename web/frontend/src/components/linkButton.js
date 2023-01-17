b66230f132db95f895e2856a8c0dad6707c63ec7
Brian Jiang <43393631+brianjiang2200@users.noreply.github.com>
2023-01-17 11:48:30 -0500

    Add Linting (#28)
    * Add Linting
    
    * Add Linting
    
    Co-authored-by: Brian Jiang <brianjiang2200@gmail.com>

--- web/frontend/src/components/linkButton.js
+++ web/frontend/src/components/linkButton.js
@@ -3,24 +3,24 @@ import { Button } from '@mui/material';
 import Link from 'next/link';
 
 export default function LinkButton(props) {
-    const {children, ...otherProps} = props
+  const {children, ...otherProps} = props
 
-    return (
-        <Link {...otherProps}>
-            <Button {...otherProps}>
-                {children}
-            </Button>
-        </Link>   
-    );
+  return (
+    <Link {...otherProps}>
+      <Button {...otherProps}>
+        {children}
+      </Button>
+    </Link>   
+  );
 }
 
 LinkButton.propTypes = {
-    href: PropTypes.string.isRequired,
-    variant: PropTypes.string,
-    sx: PropTypes.object,
-    className: PropTypes.string,
+  href: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+  sx: PropTypes.object,
+  className: PropTypes.string,
 }
 
 LinkButton.defaultProps = {
-    variant: 'contained'
+  variant: 'contained'
 }
--- web/frontend/src/constants.js
+++ web/frontend/src/constants.js
@@ -1,37 +1,37 @@
 export const terms = {
-    loading: 'Loading...'
+  loading: 'Loading...'
 }
 
 export const pages = {
-    homepage: {
-        display: true,
-        url: '/',
-        name: 'Home'
-    },
-    market: {
-        display: true,
-        url: '/market',
-        name: 'Market'
-    },
-    listings: {
-        url: '/market/listing',
-        name: 'Listing'
-    },
-    loginPage: {
-        url: '/login',
-        name: 'Login'
-    },
-    createListingPage: {
-        url: '/create',
-        name: 'Create'
-    }
+  homepage: {
+    display: true,
+    url: '/',
+    name: 'Home'
+  },
+  market: {
+    display: true,
+    url: '/market',
+    name: 'Market'
+  },
+  listings: {
+    url: '/market/listing',
+    name: 'Listing'
+  },
+  loginPage: {
+    url: '/login',
+    name: 'Login'
+  },
+  createListingPage: {
+    url: '/create',
+    name: 'Create'
+  }
 };
 
 // Light Blue, Blue, and Purple on 50% Opacity Respectively
 export const colors = [
-    "rgba(132, 206, 235, 0.5)", 
-    "rgba(90, 185, 234, 0.5)",
-    "rgba(136, 96, 208, 0.5)"
+  "rgba(132, 206, 235, 0.5)", 
+  "rgba(90, 185, 234, 0.5)",
+  "rgba(136, 96, 208, 0.5)"
 ]
 
 export const googleMapsAPIKey = 'AIzaSyClRBH_pT4waqp3BxnTtJw_7z-6clxz_HU';
